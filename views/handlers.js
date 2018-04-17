4f022078dd60a80f5ffd1359f707d1f4b63ea1f2
VeraSolo <vera_solomonova@mail.ru>
2018-04-17 15:48:58 +0300

    collapsable columns

--- views/handlers.js
+++ views/handlers.js
@@ -1,6 +1,7 @@
 
 
 function collapsePercent() {
+
  	var a = document.getElementsByClassName('param-up');
  	var b = document.getElementsByClassName('param-down');
 
@@ -30,40 +31,56 @@ function collapsePercent() {
  			bTD[i].setAttribute("style", "color: red;");
  		}
  	}
+ 	if(localStorage.getItem('collapse') == 'true') {
+ 		var c = document.getElementsByClassName('not-index');
+	 	var heads = document.getElementsByClassName('collapsable');
+	 	var colspan = ['4', '4', '5', '3'];
+		Array.prototype.slice(c);
+		Array.prototype.slice(heads);
+
+	 	for( var i = 0; i < c.length; i++ ) {
+ 			c[i].setAttribute("style", "display: none;");
+	 	}
+
+		for( var i = 0; i < heads.length; i++ ) {
+ 			heads[i].setAttribute("colspan", '1');	
+ 		}
+		
+	}
 }
 
-// function collapseNotIndex() {
-//  	var a = document.getElementsByClassName('not-index');
-//  	var heads = document.getElementsByClassName('collapsable');
-//  	var colspan = ['4', '4', '5'];
-// 	Array.prototype.slice(a);
-// 	Array.prototype.slice(heads);
+function collapseNotIndex() {
+ 	var a = document.getElementsByClassName('not-index');
+ 	var heads = document.getElementsByClassName('collapsable');
+ 	var colspan = ['4', '4', '5', '3'];
+	Array.prototype.slice(a);
+	Array.prototype.slice(heads);
 
-//  	for( var i = 0; i < a.length; i++ ) {
-//  		if (a[i].getAttribute('style') == "display: none;"){
-//  			a[i].setAttribute("style", "display: static;");
-//  			localStorage.setItem('collapse', 'false');
-//  		} else {
-//  			a[i].setAttribute("style", "display: none;");
-//  			localStorage.setItem('collapse', 'true');
-//  		}
-//  	}
-// 	if(localStorage.getItem('collapse') == 'true') {
-// 		for( var i = 0; i < heads.length; i++ ) {
-//  			heads[i].setAttribute("colspan", '1');	
-//  		}
-// 	} else {
-// 		for( var i = 0; i < heads.length; i++ ) {
-//  			heads[i].setAttribute("colspan", colspan[i]);
-//  		}
-// 	}
+ 	for( var i = 0; i < a.length; i++ ) {
+ 		if (a[i].getAttribute('style') == "display: none;"){
+ 			a[i].setAttribute("style", "display: static;");
+ 			localStorage.setItem('collapse', 'false');
+ 		} else {
+ 			a[i].setAttribute("style", "display: none;");
+ 			localStorage.setItem('collapse', 'true');
+ 		}
+ 	}
+	if(localStorage.getItem('collapse') == 'true') {
+		for( var i = 0; i < heads.length; i++ ) {
+ 			heads[i].setAttribute("colspan", '1');	
+ 		}
+	} else {
+		for( var i = 0; i < heads.length; i++ ) {
+ 			heads[i].setAttribute("colspan", colspan[i]);
+ 		}
+	}
 
-// }
+}
 
 
-// if(localStorage.getItem('collapse') == 'true') {
-// 	document.addEventListener('DOMContentLoaded', function(){
-// 		collapseNotIndex();
-// 		document.getElementById('buttonCollapsePercent').setAttribute('disable', 'true');
-// 	});
-// }
+if(localStorage.getItem('collapse') == 'true') {
+	document.addEventListener('DOMContentLoaded', function(){
+		collapseNotIndex();
+		document.getElementById('buttonCollapsePercent').setAttribute('disable', 'true');
+	});
+}
